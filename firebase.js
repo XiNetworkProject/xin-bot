ec9047d28950336acf87937992c31688c9873787
XiNetworkProject <xinetworkproject@gmail.com>
2025-03-27 23:10:46 +0100

    ✅ Correction Firebase avec Realtime Database

--- firebase.js
+++ firebase.js
@@ -1,14 +1,18 @@
+// firebase.js
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(fs.readFileSync("./xi-bot-a46b5-firebase-adminsdk-fbsvc-ff61e19670.json", "utf8"));
+// Lis la clé de service
+const serviceAccount = JSON.parse(
+  fs.readFileSync("./xi-bot-a46b5-firebase-adminsdk-fbsvc-ff61e19670.json", "utf8")
+);
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-}
+// Initialise Firebase Admin avec Realtime Database
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://xi-bot-a46b5-default-rtdb.europe-west1.firebasedatabase.app"
+});
 
-const db = admin.firestore();
+const db = admin.database(); // 👈 Utilise la Realtime DB
 
 export { db };
